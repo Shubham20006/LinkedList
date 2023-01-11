aa8ec633c538608f7ff8b1c553b8efe97c1e49cf
Shubham20006 <shubhamchaudhari1506@gmail.com>
2023-01-11 12:20:11 +0530

    Creating simple linked list

---
+++ LinkedList.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace LinkedListProgram
+{
+    public class LinkedList
+    {
+        Node head;
+
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into linkedlist",node.data);
+        }
+        public void Display()
+        {
+            Node temp = head;
+            if (head == null)
+            {
+                Console.WriteLine("linkedlist is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data);
+                temp = temp.next;
+            }
+
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,7 +10,11 @@ namespace LinkedListProgram
     {
          public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the linked list program");
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
         }
     }
 }
